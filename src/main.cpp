61da7c19be4850816e8b2ca7a1aa14b8782158e4
Tyler Jaacks <TylerJaacks@Gmail.com>
2020-10-16 14:21:52 -0500

    Added functions for calculate SIS stuff.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
 
 #ifdef __APPLE__
 #include <OpenCL/opencl.h>
@@ -9,7 +11,130 @@
 
 #define MAX_SOURCE_SIZE (0x100000)
 
-int main()
+std::vector<std::vector<float>> readSISCSVToVector(std::string fileName)
 {
-    printf("Hello, world!");
+    std::ifstream file(fileName);
+
+    std::string line, val;
+    std::vector<std::vector<float>> array;
+
+    while (std::getline (file, line)) {
+        std::vector<float> v;
+        std::stringstream s (line);
+        while (getline (s, val, ','))
+            v.push_back (std::atof(val.c_str()));
+        array.push_back (v);
+    }
+
+    return array;
+}
+
+float** multiDimensionalVectorToArray(std::vector<std::vector<float>> &vals, int N, int M)
+{
+    float** temp;
+
+    temp = new float*[N];
+
+    for(unsigned i=0; (i < N); i++)
+    {
+        temp[i] = new float[M];
+
+        for(unsigned j=0; (j < M); j++)
+        {
+            temp[i][j] = vals[i][j];
+        }
+    }
+
+    return temp;
+}
+
+float* vectorToArray(std::vector<float> &vals, int N) {
+    float* temp;
+
+    temp = new float[N];
+
+    for (unsigned i = 0; i < N; i++)
+    {
+        temp[i] = vals[i];
+    }
+
+    return temp;
+}
+
+float findAverageArray(float array[], int size)
+{
+    float sum;
+
+    for (int i = 0; i < size; i++)
+    {
+        sum += array[i];
+    }
+
+    return sum / size;
+}
+
+float* calculateSIS(float** data, int size)
+{
+    std::vector<float> sisArray;
+
+    for (int i = 1; i < size; i++)
+    {
+        for (int j = 0; j < (int) data[i][1]; j++)
+        {
+            if (data[i][0] >= 0.0f)
+            {
+                sisArray.push_back(data[i][0]);
+            }
+        }
+    }
+
+    return vectorToArray(sisArray, sisArray.size());
+}
+
+// TODO findAverageArray not working.
+float calculateSISValue(float* data, int size)
+{
+//    SIS_Value = np.average(SIS_Array)
+
+
+    return findAverageArray(data, size);
+}
+
+void calculateSISNorm()
+{
+//    SIS_Array_Norm = np.true_divide(SIS_Array, SIS_Value)
+}
+
+// TODO This code should run with OpenCL in the future.
+void calculateMonteCarlo(float countRate[])
+{
+
+}
+
+int main(int argc, char* args[])
+{
+    printf("Reading from SIS CSV file...\n");
+
+    // TODO Load this from the command line arguments.
+    std::string fileName = "/Users/tylerjaacks/Desktop/TestData/ISUSummedData.csv";
+
+    // TODO Make this is one operation i.e .CSV -> float** or float[][].
+    std::vector<std::vector<float>> data = readSISCSVToVector(fileName);
+    int size = data.size();
+    float** dataArray = multiDimensionalVectorToArray(data, data.size(), 2);
+
+    printf("Done Reading from SIS.\n");
+
+    printf("Calculating SIS Values...\n");
+
+    float* sis = calculateSIS(dataArray, size);
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("&f", sis[i]);
+    }
+
+    float sisValue = calculateSISValue(sis, size);
+
+    printf("sisValue: %f", sisValue);
 }
